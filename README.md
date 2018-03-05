caa5e94744b0e55ee2688cc57166e638dafac617
xataz <xataz@mondedie.fr>
2018-03-05 20:13:40 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# Archlinux
+
+## Repository
+* i3-gaps i3blocks i3lock i3status
+* rofi
+* cmus
+* otf-font-awesome
+* termite
+* vim
+* compton
+* zsh
+
+## AUR
+* polybar
+* code
+* trizen
+* nextcloud-client
+
+## Manual
+* oh-my-zsh
---
+++ script_copie.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+HOME_PATH=~/
+CONFIG_FILE="${HOME_PATH}/.config/polybar \
+            ${HOME_PATH}/.config/compton \
+            ${HOME_PATH}/.config/i3 \
+            ${HOME_PATH}/.config/rofi \
+            ${HOME_PATH}/.config/termite \
+            ${HOME_PATH}/.i3/wallpaper.jpg \
+            ${HOME_PATH}/.gnupg/gpg.conf \
+            ${HOME_PATH}/.vim/vimrc"
+
+
+for i in $CONFIG_FILE; do
+    cp -R $i $(dirname $i | sed 's#'${HOME_PATH}'/##')
+done
